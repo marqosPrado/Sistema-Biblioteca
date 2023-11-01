d310a39e3ae5c2fe4d139eb8280730af4d0d2a4c
Oldiee_ <oldiee@localhost.localdomain>
2023-11-01 18:31:12 -0300

    entidade 'Customer' criada e com todos seus atributos definidos

--- src/main/java/br/com/bibliotecasaber/sistemagestaobiblioteca/entities/Customer.java
+++ src/main/java/br/com/bibliotecasaber/sistemagestaobiblioteca/entities/Customer.java
@@ -1,6 +1,10 @@
 package br.com.bibliotecasaber.sistemagestaobiblioteca.entities;
 
+import br.com.bibliotecasaber.sistemagestaobiblioteca.enums.Role;
+import br.com.bibliotecasaber.sistemagestaobiblioteca.enums.Status;
 import jakarta.persistence.*;
+
+import java.util.List;
 
 @Entity
 @Table(name = "customer")
@@ -8,10 +12,81 @@ public class Customer {
 
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
-    private Long id;
+    @Column(name = "customer_id")
+    private Long customerId;
+    @Column(name = "first_name")
     private String firstName;
+    @Column(name = "last_name")
     private String lastName;
     private String email;
 
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "customer")
+    private List<Adress> adress;
 
+    @Enumerated(EnumType.STRING)
+    private Status status;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    public Customer(String firstName, String lastName,
+                    String email, List<Adress> adress, Status status, Role role) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.adress = adress;
+        this.status = status;
+        this.role = role;
+    }
+
+    public Customer() {
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public List<Adress> getAdress() {
+        return adress;
+    }
+
+    public void setAdress(List<Adress> adress) {
+        this.adress = adress;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
 }
