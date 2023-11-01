ce27aa3592fc7c4fecfb7dfdf02b8b003bd7c1aa
Oldiee_ <oldiee@localhost.localdomain>
2023-11-01 17:53:09 -0300

    nenhuma alteração

--- src/main/java/br/com/bibliotecasaber/sistemagestaobiblioteca/SistemaGestaoBibliotecaApplication.java
+++ src/main/java/br/com/bibliotecasaber/sistemagestaobiblioteca/SistemaGestaoBibliotecaApplication.java
@@ -2,10 +2,13 @@ package br.com.bibliotecasaber.sistemagestaobiblioteca;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 public class SistemaGestaoBibliotecaApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(SistemaGestaoBibliotecaApplication.class, args);
     }
