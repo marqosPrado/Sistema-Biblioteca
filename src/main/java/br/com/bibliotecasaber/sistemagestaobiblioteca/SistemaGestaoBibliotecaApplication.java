e0ce4ff76a5cf3f311cfbd35d179625b9f0317e7
Oldiee_ <oldiee@localhost.localdomain>
2023-11-01 16:57:01 -0300

    primeiro commit

---
+++ src/main/java/br/com/bibliotecasaber/sistemagestaobiblioteca/SistemaGestaoBibliotecaApplication.java
@@ -0,0 +1,13 @@
+package br.com.bibliotecasaber.sistemagestaobiblioteca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaGestaoBibliotecaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SistemaGestaoBibliotecaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/bibliotecasaber/sistemagestaobiblioteca/SistemaGestaoBibliotecaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.bibliotecasaber.sistemagestaobiblioteca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaGestaoBibliotecaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
