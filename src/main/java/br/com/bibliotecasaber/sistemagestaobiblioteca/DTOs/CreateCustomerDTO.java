0a64457e545e4bd5665f76e594a62cc1571d2e89
Oldiee_ <oldiee@localhost.localdomain>
2023-11-02 15:08:24 -0300

    implementado as validações nos atributos firstName, lastName, email, addresses

--- src/main/java/br/com/bibliotecasaber/sistemagestaobiblioteca/DTOs/CreateCustomerDTO.java
+++ src/main/java/br/com/bibliotecasaber/sistemagestaobiblioteca/DTOs/CreateCustomerDTO.java
@@ -2,9 +2,19 @@ package br.com.bibliotecasaber.sistemagestaobiblioteca.DTOs;
 
 import br.com.bibliotecasaber.sistemagestaobiblioteca.entities.Address;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
 import java.util.List;
 
 public record CreateCustomerDTO(
-        String firstName, String lastName, String email, List<Address> addresses
+        @NotBlank(message = "First name is mandatory")
+        String firstName,
+        @NotBlank(message = "Last name is mandatory")
+        String lastName,
+        @Email(message = "Email is not valid")
+        String email,
+        @NotBlank(message = "Address is mandatory")
+        List<Address> addresses
 ) {
+
 }
