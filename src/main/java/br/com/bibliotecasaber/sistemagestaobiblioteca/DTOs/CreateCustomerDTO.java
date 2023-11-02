7712fa192cea41de501a9e2cba16c4b3d4fa2a86
Oldiee_ <oldiee@localhost.localdomain>
2023-11-02 14:48:18 -0300

    concluida a implementação do CreateCustomerDTO

--- src/main/java/br/com/bibliotecasaber/sistemagestaobiblioteca/DTOs/CreateCustomerDTO.java
+++ src/main/java/br/com/bibliotecasaber/sistemagestaobiblioteca/DTOs/CreateCustomerDTO.java
@@ -4,5 +4,7 @@ import br.com.bibliotecasaber.sistemagestaobiblioteca.entities.Address;
 
 import java.util.List;
 
-public record CreateCustomerDTO(String firstName, String lastName, String email, List<Address> addresses) {
+public record CreateCustomerDTO(
+        String firstName, String lastName, String email, List<Address> addresses
+) {
 }
